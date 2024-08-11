4afc99ab374f09fa3171090fa7aa034914879f8e
razzput007 <dhoom.rk74@gmail.com>
2024-08-11 13:39:10 +0530

    first commit

---
+++ src/model/userSchema.ts
@@ -0,0 +1,64 @@
+import mongoose, { Schema } from "mongoose";
+import validator from 'validator'
+
+interface IUser extends Document{
+  name: string;
+  _id:string;
+  email: string;
+  role:"admin"|"user";
+  photo:string;
+  gender: "male" | "female";
+  dob: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  age: number;
+
+}
+const userSchema=new Schema({
+  _id:{
+    type:String,
+    required:[true,"Please enter ID"]
+  },
+  name:{
+    type:String,
+    required:[true,"Please enter name"]
+  },
+  email:{
+    type:String,
+    required:[true,"Please enter email"],
+    unique:[true,"Email Already Exist"],
+    validate:validator.default.isEmail
+  },
+  photo:{
+    type:String,
+    required:[true,"Please add photo"]
+  },
+  role:{
+    type:String,
+    enum:["admin","user"],
+    default:"user"
+  },
+  gender:{
+    type:String,
+    enum:["male","female"],
+    required:[true,"please enter Gender"]
+  },
+  dob:{
+    type:Date,
+    required:[true,"please enter Date Of Birth"]
+  }
+},{timestamps:true});
+
+userSchema.virtual("age").get(function(){
+  const today = new Date();
+  const birthDate = new Date(this.dob);
+  let age = today.getFullYear() - birthDate.getFullYear();
+   if(today.getMonth()<birthDate.getMonth() || today.getMonth()===birthDate.getMonth() && today.getDate()<birthDate.getDate()){
+    age--;
+   }
+
+   return age;
+  
+})
+
+export const user=mongoose.model<IUser>("user",userSchema);
