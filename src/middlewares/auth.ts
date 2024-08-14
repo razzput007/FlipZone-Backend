ec6212f79ae5b7c83d4e659efa811403198a59d7
razzput007 <dhoom.rk74@gmail.com>
2024-08-14 14:51:50 +0530

    completed auth

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,19 @@
+import { TryCatch } from "./error.js";
+import { User } from "../model/userSchema.js";
+import ErrorHandler from "../utils/utility-class.js";
+
+export const adminOnly=TryCatch(async(req,res,next)=>{
+    const id=req.query.id;
+    if(!id){
+        return next(new ErrorHandler("Please Login In",401));
+    }
+    const user=await User.findById(id);
+    if(!user){
+     return next(new ErrorHandler("Inavlid Id",401));
+    }
+
+    if(user.role!=="admin"){
+     return next(new ErrorHandler("Invalid Role",401));
+    }
+    next();
+})
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { newUser ,getAllUser,getUser,deleteUser} from "../controllers/userController.js";
-
+import { adminOnly } from "../middlewares/auth.js";
 const app=express.Router();
 
 app.post("/new",newUser);
 
 app.get("/all",getAllUser);
 
-app.get("/:id",getUser);
+app.get("/:id",adminOnly,getUser);
 
-app.delete("/:id",deleteUser);
+app.delete("/:id",adminOnly,deleteUser);
 
 export default app;
