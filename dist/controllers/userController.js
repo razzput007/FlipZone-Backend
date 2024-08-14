49779d9e90e64fabe0ed36df247e7340900f3b9b
razzput007 <dhoom.rk74@gmail.com>
2024-08-14 13:20:18 +0530

    Making user controller

--- dist/controllers/userController.js
+++ dist/controllers/userController.js
@@ -1,28 +1,19 @@
 import { User } from "../model/userSchema.js";
-import ErrorHandler from "../utils/utility-class.js";
-export const newUser = async (req, res, next) => {
-    try {
-        return next(new ErrorHandler("Error in User Controller", 400));
-        const { name, email, photo, gender, role, _id, dob } = req.body;
-        console.log(name, email, photo, gender, role, _id, dob);
-        const user = await User.create({
-            name,
-            email,
-            photo,
-            role,
-            gender,
-            _id,
-            dob: new Date(dob)
-        });
-        return res.status(200).json({
-            status: "success",
-            message: `Welcome ${user.name}`
-        });
-    }
-    catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: error
-        });
-    }
-};
+import { TryCatch } from "../middlewares/error.js";
+export const newUser = TryCatch(async (req, res, next) => {
+    const { name, email, photo, gender, role, _id, dob } = req.body;
+    console.log(name, email, photo, gender, role, _id, dob);
+    const user = await User.create({
+        name,
+        email,
+        photo,
+        role,
+        gender,
+        _id,
+        dob: new Date(dob)
+    });
+    return res.status(200).json({
+        status: "success",
+        message: `Welcome ${user.name}`
+    });
+});
